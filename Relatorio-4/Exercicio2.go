503c214c4b55c303e89f8235172f0daed7dea0e3
Leonardo Fernandes <Leonardo321fernandes@outlook.com>
2026-09-10 21:05:08 -0300

    Add sales input and classification logic

--- Relatorio-4/Exercicio2.go
+++ Relatorio-4/Exercicio2.go
@@ -1,1 +1,32 @@
+package main
 
+import "fmt"
+
+func main() {
+	var t1, t2, t3 int
+
+	fmt.Print("Digite as vendas do 1º trimestre: ")
+	fmt.Scanln(&t1)
+	fmt.Print("Digite as vendas do 2º trimestre: ")
+	fmt.Scanln(&t2)
+	fmt.Print("Digite as vendas do 3º trimestre: ")
+	fmt.Scanln(&t3)
+
+	total := t1 + t2 + t3
+
+	if total < 100 {
+		fmt.Println("Meta mínima anual não atingida!")
+	} else {
+		fmt.Println("")
+		fmt.Println("Total de vendas:", total, "unidades")
+
+		switch {
+		case total >= 250:
+			fmt.Println("Classificação: Categoria Top Seller")
+		case total >= 180:
+			fmt.Println("Classificação: Categoria Sênior")
+		default:
+			fmt.Println("Classificação: Categoria Pleno")
+		}
+	}
+}
