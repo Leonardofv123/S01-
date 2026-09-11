74450d66abe628e7ae59b57b8594fc247010432a
Leonardo Fernandes <Leonardo321fernandes@outlook.com>
2026-09-10 21:05:27 -0300

    Add function to generate on-call schedule

--- Relatorio-4/Exercicio3.go
+++ Relatorio-4/Exercicio3.go
@@ -1,1 +1,22 @@
+package main
 
+import "fmt"
+
+func gerarEscalaPlantao(n int) {
+	fmt.Println("")
+	fmt.Println("--- Escala de Plantão Técnico ---")
+
+	for i := 1; i <= n; i++ {
+		dia := 1 + (i-1)*4
+		fmt.Printf("Plantão %d: Dia %d do mês\n", i, dia)
+	}
+}
+
+func main() {
+	var n int
+
+	fmt.Print("Digite a quantidade de plantões necessários: ")
+	fmt.Scanln(&n)
+
+	gerarEscalaPlantao(n)
+}
